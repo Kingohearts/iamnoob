d939b979de80d94269f0018a94b42767fb847b22
Kingohearts <sudhamsu.24@gmail.com>
2023-09-08 20:36:28 +0530

    with Rest call

--- src/main/java/com/example/iamnoob/NoobController.java
+++ src/main/java/com/example/iamnoob/NoobController.java
@@ -1,6 +1,7 @@
 package com.example.iamnoob;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +12,7 @@ public class NoobController {
 	NoobService ns;
 	
 	@PostMapping
-	public OutputBean postHandler(InputBean in) {
+	public ResponseEntity<?> postHandler(InputBean in) {
 		
 		return ns.handlePostRequest(in);
 	}
--- src/main/java/com/example/iamnoob/NoobService.java
+++ src/main/java/com/example/iamnoob/NoobService.java
@@ -1,11 +1,32 @@
 package com.example.iamnoob;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.google.gson.Gson;
 
 @Service
 public class NoobService {
+	private static final Logger logger = LogManager.getLogger(NoobService.class);
+	Gson gs = new Gson();
 
-	OutputBean handlePostRequest(InputBean in) {
-		return MapInputOutput.INSTACE.convertToOutputBean(in);
+	ResponseEntity<?> handlePostRequest(InputBean in) {
+		logger.info("printBegin");
+		OutputBean out = MapInputOutput.INSTACE.convertToOutputBean(in);
+		String outStr = gs.toJson(out);
+		RestTemplate rs = new RestTemplate();
+		String url = "https://dummy.restapiexample.com/api/v1/create";
+		HttpEntity<OutputBean> req = new HttpEntity<OutputBean>(out);
+		ResponseEntity<?> response = rs.exchange(url, HttpMethod.POST, req, OutputBean.class);
+		logger.info("PRINTING");
+		logger.info(outStr);
+
+		//return out;
+		return response;
 	}
 }
